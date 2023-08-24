3eab697ec078c7359bff297697b1cba4eb97e741
Vitalii Dutov <dutov.vit.vic@gmail.com>
2023-08-24 05:50:04 +0300

    reserve push

--- src/app/components/main-content/how-it-works/how-it-works.component.ts
+++ src/app/components/main-content/how-it-works/how-it-works.component.ts
@@ -9,38 +9,35 @@ export class HowItWorksComponent {
   cards = [
     {
       title: 'Оберіть психолога який Вам сподобався',
+      img: '../../../../assets/how-it-works-item__img_1.svg',
       content:
         'Ми пропонуємо лише кваліфікованих спеціалістів, які проходять чіткий контроль своїх знань та навичок.',
     },
     {
       title: 'Оберіть місце і час',
+      img: '../../../../assets/how-it-works-item__img_2.svg',
       content:
         'Обирайте комфортне для себе місце та час консультації, скористуйтеся онлайн-календарем фахівця. Ви можете змінити дату та час за 24 години безкоштовно.',
     },
     {
       title: 'Підтвердіть сеанс',
+      img: '../../../../assets/how-it-works-item__img_3.svg',
       content:
         'Оплатіть консультацію і ми надішлемо підтвердження та нагадування за день до сеансу.',
     },
     {
       title: 'Керуйте процесом',
+      img: '../../../../assets/how-it-works-item__img_4.svg',
       content:
         'Керуйте записами в особистому кабінеті, переглядайте свою історію сеансів та з легкістю плануйте наступні.',
     },
   ];
 
-  cardsImgs = [
-    '../../../../assets/how-it-works-item__img_1.svg',
-    '../../../../assets/how-it-works-item__img_2.svg',
-    '../../../../assets/how-it-works-item__img_3.svg',
-    '../../../../assets/how-it-works-item__img_4.svg',
-  ];
-
   getClasses(index: number) {
     if (index % 2 === 0) {
-      return 'how-it-works-item-even';
+      return 'odd';
     } else {
-      return 'how-it-works-item-odd';
+      return 'even';
     }
   }
 }
