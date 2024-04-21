17c0a49410ec8f0d3fdf04b4f6ba98a8f2988fb0
Vitalii Dutov <dutov.vit.vic@gmail.com>
2024-04-21 13:21:26 +0300

    introductory build

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { ActivatedRoute, UrlSegment } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, UrlSegment } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -9,20 +10,30 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(public authService: AuthService, private route: ActivatedRoute) {}
+  constructor(
+    public authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   @ViewChild('nav', { static: true }) nav!: ElementRef;
   @ViewChild('loginBtn', { static: true }) loginBtn!: ElementRef;
 
   isMobileNavOpen: boolean = false;
-
   isUserProfile: boolean = true;
 
   ngOnInit(): void {
-    const urlSegments: UrlSegment[] = this.route.snapshot.url;
-    this.isUserProfile = urlSegments
-      .map((segment) => segment.path)
-      .includes('profile');
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentPageUrl = event.urlAfterRedirects;
+        console.log(currentPageUrl);
+        if (currentPageUrl === '/user-page') {
+          this.isUserProfile = true;
+        } else {
+          this.isUserProfile = false;
+        }
+      }
+    });
   }
 
   mobileNav() {
